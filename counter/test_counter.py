8108a17fcd65acbf987084881252362ddd2b1327
Patarimi <mpqqch@gmail.com>
2023-04-20 12:15:48 +0200

    Counter can now increment by any number on each step

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -4,12 +4,22 @@ from cocotb.clock import Clock
 
 
 @cocotb.test()
-async def fonctionnal_test(dut):
+async def fonctionnal_test_1(dut):
+    await fonctionnal_test_n(dut, 1)
+
+
+@cocotb.test()
+async def fonctionnal_test_2(dut):
+    await fonctionnal_test_n(dut, 2)
+
+
+async def fonctionnal_test_n(dut, incr):
     cocotb.start_soon(Clock(dut.clk, 1, units="ns").start())
     dut.reset.value = 1
+    dut.incr.value = incr
     await Timer(1, units="ns")
     dut.reset.value = 0
     await Timer(20, units="ns")
     await FallingEdge(dut.clk)
     dut._log.info(f"{dut.count.value=}")
-    assert dut.count.value == 21
+    assert dut.count.value == 21 * incr
